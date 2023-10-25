1ab4f3d4dffa6ae07707de73a10e6389884c3670
aqnuchan <138773060+aqnuchan@users.noreply.github.com>
2023-10-25 17:14:31 +0600

    Add files via upload

---
+++ EngineHelpers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Drawing;
+
+namespace ExpressedEngine.ExpressedEngine
+{
+    public  class EngineHelpers
+    {
+        private static Bitmap spriteBitmap = null;
+        private static Image spriteImage = null;
+
+        public static Bitmap LoadImageInMemory(string directory)
+        {
+         
+            try
+            {
+                spriteImage = Image.FromFile($"{directory}");
+               
+            }
+            catch (Exception imgNotFound)
+            {
+                Log.Error($"{imgNotFound.StackTrace}");
+                
+            }
+            finally
+            {
+                spriteBitmap = new Bitmap(spriteImage);
+              
+            }
+            return spriteBitmap;
+        
+        }
+    }
+}
